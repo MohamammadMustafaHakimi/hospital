8e188e4f6d4189f81431321189de0f412d44fd50
Mohammad Mustafa Hakimi <mmustafahakimi08@gmail.com>
2023-11-21 13:28:41 +0100

    association for education is made

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,6 +1,5 @@
 class Education < ApplicationRecord
-
-  belongs_to :doctor #have to specify the destroy dependency
+  belongs_to :doctor, dependent: :destroy
   validates :university, presence: true
   validates :degree_name, presence: true
   validates :score, numericality: {greater_than_or_equal_to: 50, less_than_or_equal_to: 100}
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,4 @@
 class Patient < ApplicationRecord
-  belongs_to :doctor
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :insurance, presence: true, inclusion: { in: %w(Private Public)}
