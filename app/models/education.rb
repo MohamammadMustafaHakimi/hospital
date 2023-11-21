422046cec331b0995b9f8b2d588776eb76725e6b
Mohammad Mustafa Hakimi <mmustafahakimi08@gmail.com>
2023-11-21 13:25:55 +0100

    validation for education is made without association with doctor

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,3 +1,8 @@
 class Education < ApplicationRecord
-  belongs_to :doctor
+
+  belongs_to :doctor #have to specify the destroy dependency
+  validates :university, presence: true
+  validates :degree_name, presence: true
+  validates :score, numericality: {greater_than_or_equal_to: 50, less_than_or_equal_to: 100}
+
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,5 +3,4 @@ class Patient < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :insurance, presence: true, inclusion: { in: %w(Private Public)}
-  validates :score, numericality: { greater_than_or_equal_to: 50, less_than_or_equal_to: 100 }
 end
